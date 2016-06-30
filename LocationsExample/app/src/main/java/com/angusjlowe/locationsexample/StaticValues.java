84d0d0968eaea67aeb79cb4e23c51e796bcf5022
Angus Lowe <anguslowe@live.ca>
2016-06-30 17:36:56 +0900

    fixed crashes on locationsexample and added remaining core functionality (allowing the user to add locations indefinitely and update the firebase backend using a consecutively increasing unique id). Crashes were due to reconnecting to firebase in mapsactivity, so a separate class was created to store the static values from running the mainactivity (which connects to firebase).

---
+++ LocationsExample/app/src/main/java/com/angusjlowe/locationsexample/StaticValues.java
@@ -0,0 +1,13 @@
+package com.angusjlowe.locationsexample;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Created by Angus on 2016-06-30.
+ */
+public class StaticValues {
+    public static HashMap<String, LatLng> namesAndCoords;
+    public static int largestId = 0;
+}
