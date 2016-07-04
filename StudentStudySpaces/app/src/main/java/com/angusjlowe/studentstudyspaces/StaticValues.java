ebd01ffdc98d10a29b6cd7e4b14ba0a4ab3afd04
Angus Lowe <anguslowe@live.ca>
2016-07-04 09:25:39 +0900

    added first prototype of front-end android application with testing features

---
+++ StudentStudySpaces/app/src/main/java/com/angusjlowe/studentstudyspaces/StaticValues.java
@@ -0,0 +1,14 @@
+package com.angusjlowe.studentstudyspaces;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Created by Angus on 2016-07-01.
+ */
+public class StaticValues {
+    public static HashMap<String, LatLng> namesAndCoords;
+    public static int largestStudySpaceId = 0;
+    public static int largestCommentId = 0;
+}
