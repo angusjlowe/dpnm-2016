44ee9e9f44d9039a493ac3161ae176e25eea77d8
Angus Lowe <anguslowe@live.ca>
2016-07-05 17:29:00 +0900

    actually added files from last commit...

--- StudentStudySpaces/app/src/main/java/com/angusjlowe/studentstudyspaces/MainActivity.java
+++ StudentStudySpaces/app/src/main/java/com/angusjlowe/studentstudyspaces/MainActivity.java
@@ -1,5 +1,7 @@
 package com.angusjlowe.studentstudyspaces;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,15 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editTextLocation;
     private EditText editTextName;
-    private Button buttonAddLocation;
-    private TextView textViewLocations;
-    ArrayList<Integer> studySpaceIds = new ArrayList<>();
-    ArrayList<Integer> commentIds = new ArrayList<>();
+    Button buttonAddLocation;
+    String alertDialogMessage;
     Firebase ref = new Firebase("https://studentstudyspaces.firebaseio.com/");
     Firebase studySpaces = ref.child("study spaces");
-    SeekBar seekBar;
-    TextView textViewRating;
-    EditText editTextComment;
 
 
     @Override
@@ -42,10 +39,6 @@ public class MainActivity extends AppCompatActivity {
         editTextLocation = (EditText) findViewById(R.id.editTextLocation);
         editTextName = (EditText) findViewById(R.id.editTextName);
         buttonAddLocation = (Button) findViewById(R.id.buttonAddLocation);
-        textViewLocations = (TextView) findViewById(R.id.textViewLocations);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        textViewRating = (TextView) findViewById(R.id.textViewRating);
-        editTextComment = (EditText) findViewById(R.id.editTextComment);
 
         studySpaces.addValueEventListener(new ValueEventListener() {
             @Override
@@ -59,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
                     String name = (String) details.get("name");
                     String location = (String) details.get("location");
                     DataSnapshot comments = dataSnapshot.child("comments");
-                    detailsString += comments.getKey();
                     for(DataSnapshot comment : comments.getChildren()) {
                         Map<String, Object> commentDetails = comment.getValue(Map.class);
                         for(String attribute : commentDetails.keySet()) {
@@ -67,12 +59,8 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     detailsString += name + ": " + location + " ";
-                    textViewLocations.setText(detailsString);
+                    alertDialogMessage = detailsString;
 
-                    //find max id number of study spaces
-                    String studySpaceIdString = dataSnapshot.getKey();
-                    Integer studySpaceId = Integer.parseInt(studySpaceIdString);
-                    studySpaceIds.add(studySpaceId);
 
                     //format study space details and add to hashmap for markers
                     location.replaceAll(" ", "");
@@ -93,24 +81,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress;
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                progress = i;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                textViewRating.setText(String.valueOf(progress));
-            }
-        });
 
         buttonAddLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,34 +89,21 @@ public class MainActivity extends AppCompatActivity {
                 //update database according to user input - add study space attributes
                 String stringLocation = editTextLocation.getText().toString();
                 String stringName = editTextName.getText().toString();
-                String stringComment = editTextComment.getText().toString();
-                String stringRating  = String.valueOf(seekBar.getProgress());
                 Map<String, Object> details = new HashMap<String, Object>();
                 Map<String, Object> studySpacesMap = new HashMap<String, Object>();
-                Map<String, Object> commentDetails = new HashMap<String, Object>();
-                Map<String, Object> comments = new HashMap<String, Object>();
                 details.put("location", stringLocation);
                 details.put("name", stringName);
-                details.put("rating", stringRating);
-                //add comment attributes
-                commentDetails.put("content", stringComment);
-                commentDetails.put("date", "07/01/2017/14:19");
-                commentDetails.put("votes", "0");
+                details.put("rating", "");
+                details.put("image", "");
+                details.put("decibel", "");
+                details.put("decibel_list", "");
+                details.put("rating_list", "");
 
-                //determine the largest study space id before adding a new one
-                Integer[] studySpaceIdsArray = studySpaceIds.toArray(new Integer[studySpaceIds.size()]);
-                for(Integer i : studySpaceIdsArray) {
-                    if(i > StaticValues.largestStudySpaceId) {
-                        StaticValues.largestStudySpaceId = i;
-                    }
-                }
 
-                //add nested comments attributes
-                comments.put("1", commentDetails);
-                details.put("comments", comments);
+
                 //update study space
-                studySpacesMap.put(String.valueOf(StaticValues.largestStudySpaceId + 1), details);
-                studySpaces.updateChildren(studySpacesMap);
+                studySpaces.push().setValue(details);
+
 
 
                 Toast.makeText(getApplicationContext(), "Database updated successfully", Toast.LENGTH_LONG).show();
@@ -155,9 +112,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void serverCheck(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this).setTitle("Firebase JSON").setMessage(alertDialogMessage);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+
+    }
+
 
     public void goToMap(View v) {
         Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToAddComments(View v) {
+        Intent intent = new Intent(this, AddComments.class);
         startActivity(intent);
     }
 }
