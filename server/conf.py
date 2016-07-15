a4cace6ab2082ac20b07bc8de92ed2983c0e545c
Joey Pate <patex019@umn.edu>
2016-07-15 09:41:08 +0900

    added the beegees to the server

--- server/conf.py
+++
@@ -1,6 +0,0 @@
-config = {
-	"apiKey": "AIzaSyCn71iAs65uXTqaOL0iz0aC7hu2r9_K4b8",
-	"authDomain": "studentstudyspaces.firebaseapp.com",
-	"databaseURL": "https://studentstudyspaces.firebaseio.com/",
-	"storageBucket": "studentstudyspaces.appspot.com"
-	}
