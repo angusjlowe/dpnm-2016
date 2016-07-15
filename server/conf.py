e27ff6745fc7761424ad361eec15ecfbfcaa9aec
Joey Pate <patex019@umn.edu>
2016-07-15 09:40:16 +0900

    hopeful update to the server

---
+++ server/conf.py
@@ -0,0 +1,6 @@
+config = {
+	"apiKey": "AIzaSyCn71iAs65uXTqaOL0iz0aC7hu2r9_K4b8",
+	"authDomain": "studentstudyspaces.firebaseapp.com",
+	"databaseURL": "https://studentstudyspaces.firebaseio.com/",
+	"storageBucket": "studentstudyspaces.appspot.com"
+	}
