48da862053e7bcbff6862594c09c04e8154e119c
swissme8888 <hyeonseo777yun@gmail.com>
2016-07-18 09:48:04 +0200

    adding mapdemo

---
+++ mapdemo/app/src/main/java/com/example/hyeonseo/mapdemo/Fragment/MainFragment.java
@@ -0,0 +1,22 @@
+package com.example.hyeonseo.mapdemo.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hyeonseo.mapdemo.R;
+
+/**
+ * Created by HyeonSeo on 7/18/2016.
+ */
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+}
